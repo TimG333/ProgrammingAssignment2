0f82b603bc96d9d81687f6e1fa7065261a1d64d9
TimG333 <tim.groth@gmail.com>
2014-04-24 00:32:45 -0400

    Peer Assignment Attempt One
    Attempting programming assignment two, peer assessment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As per the assignment the functions create and solve a matrix that allows
+## for a cached solution
 
-## Write a short comment describing this function
+## Creates a list of functions to allow the caching to work
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrixinverse <- NULL
+  set <- function(y) {
+    x <<- y
+    matrixinverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) matrixinverse <<- inverse
+  getInverse <- function() matrixinverse
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Uses the list from makeCacheMatrix to solve or retrieve a Matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matrixinverse <- x$getInverse()
+  if(!is.null(matrixinverse)) {
+    message("getting cached data")
+    return(matrixinverse)
+  } 
+  data <- x$get()
+  matrixinverse <- solve(data, ...)
+  x$setInverse(matrixinverse)
+  matrixinverse
 }
